04406ce6fc7d5002143173ee72fe9cd4ab15b829
jjcasanova <jjcasanova@gmail.com>
2016-05-06 23:57:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jjcasanova/arduinoluces/MainActivityArduinoLuces.java
@@ -0,0 +1,13 @@
+package com.example.jjcasanova.arduinoluces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityArduinoLuces extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_arduino_luces);
+    }
+}
